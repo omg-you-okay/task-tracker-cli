231ee1823db6555bb5fe41fdeeb402350586537d
omg-you-okay <omg.you.okay@gmail.com>
2026-01-16 02:57:51 +0000

    implement adding

--- Task Tracker CLI/Task.cs
+++ Task Tracker CLI/Task.cs
@@ -13,6 +13,6 @@ public class Task
   public required string Id { get; set; }
   public required string Description { get; set; }
   public Status Status { get; set; } = Status.Todo;
-  public DateTime CreatedAt { get; set; }
+  public DateTime? CreatedAt { get; set; }
   public DateTime UpdatedAt { get; set; }
 }
--- Task Tracker CLI/TaskTracker.cs
+++ Task Tracker CLI/TaskTracker.cs
@@ -1,27 +1,66 @@
+using System.Text.Json;
+
 namespace Task_Tracker_CLI;
 
 public static class TaskTracker
 {
   public static void AddTask(string description)
   {
-    Console.WriteLine($"adding {description}");
+    var pathToFile = Path.GetFullPath(Path.Join(Directory.GetCurrentDirectory(), "temp.txt"));
+
+    var task = new Task()
+    {
+      Id = "1",
+      Description = description,
+      CreatedAt = DateTime.Now,
+      Status = Status.Todo,
+    };
+
+    if (!File.Exists(pathToFile))
+    {
+      List<Task> tasks = [task];
+      File.WriteAllText(pathToFile, JsonSerializer.Serialize(tasks));
+      return;
+    }
+
+    var readFile = File.ReadAllText(pathToFile);
+    var readTasks = JsonSerializer.Deserialize<List<Task>>(readFile);
+    if (readTasks == null) return;
+
+    List<int> ids = [];
+
+    foreach (var readTask in readTasks)
+    {
+      ids.Add(int.Parse(readTask.Id));
+    }
+    ids.Sort();
+    var newId = (ids.LastOrDefault() + 1).ToString();
+    task.Id = newId;
+    readTasks.Add(task);
+
+    File.WriteAllText(pathToFile, JsonSerializer.Serialize(readTasks));
+    
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write($"Id: {newId}; Added: ");
+    Console.ResetColor();
+    Console.WriteLine(description);
   }
 
   public static void UpdateTask(int id, string description)
   {
     Console.WriteLine($"updating task: {id} with: {description}");
   }
-  
+
   public static void DeleteTask(int id)
   {
     Console.WriteLine($"deleting task: {id}");
   }
-  
+
   public static void MarkInProgress(int id)
   {
     Console.WriteLine($"now in-progress: {id}");
   }
-  
+
   public static void MarkDone(int id)
   {
     Console.WriteLine($"now done: {id}");
@@ -34,8 +73,8 @@ public static class TaskTracker
       var status = StatusToEnumMapper(arguments[2]);
       ListByStatus(status);
       return;
-    } 
-    
+    }
+
     Console.WriteLine("listing all items");
   }
 
