b40bcc217e3f00d6b9918458d805dd663b33ca16
omg-you-okay <omg.you.okay@gmail.com>
2026-01-23 03:37:34 +0000

    iDesing complete

--- Task Tracker CLI/Program.cs
+++ Task Tracker CLI/Program.cs
@@ -15,182 +15,33 @@ var command = arguments[1];
 
 var taskRepository = new TaskRepositoryResource(Path.Join(Directory.GetCurrentDirectory(), "tasks.json"));
 var taskEngine = new TaskEngine();
-var taskTracker = new TaskManager(taskRepository, taskEngine);
+var taskManager = new TaskManager(taskRepository, taskEngine);
 
 
 switch (command)
 {
   case "add":
-    if (arguments.Length < 3)
-    {
-      Errors.CustomError("text is missing");
-      return;
-    }
-
-    var addResult = taskTracker.AddTask(arguments[2]);
-
-    switch (addResult.operationResult)
-    {
-      case OperationResult.Success:
-        Errors.PrintAddInfo(addResult.task!.Id, addResult.task!.Description);
-        break;
-      case OperationResult.EmptyDescription:
-        Console.WriteLine("task description can't be empty");
-        break;
-    }
-
+    AddTask();
     break;
 
   case "update":
-
-    if (!taskTracker.HasTasks())
-    {
-      Console.WriteLine("Add at leat 1 task");
-      return;
-    }
-
-    switch (arguments.Length)
-    {
-      case < 3:
-        Errors.NoId();
-        return;
-      case < 4:
-        Errors.CustomError("text is missing");
-        return;
-    }
-
-    var (operationResult, task) = taskTracker.UpdateTask(int.Parse(arguments[2]), arguments[3]);
-
-    switch (operationResult)
-    {
-      case OperationResult.NotFound:
-        Errors.TaskNotFound(arguments[2]);
-        break;
-      case OperationResult.Success:
-        Errors.PrintTaskInfo(task!);
-        break;
-    }
-
+    UpdateTask();
     break;
 
   case "delete":
-
-    if (!taskTracker.HasTasks())
-    {
-      Console.WriteLine("Add at leat 1 task");
-      return;
-    }
-
-    if (arguments.Length < 3)
-    {
-      Errors.NoId();
-      return;
-    }
-
-    var deleteResult = taskTracker.DeleteTask(Int32.Parse(arguments[2]));
-
-    switch (deleteResult.operationResult)
-    {
-      case OperationResult.NotFound:
-        Errors.TaskNotFound(arguments[2]);
-        break;
-      case OperationResult.Success:
-        Errors.CustomError($"you removed task: {deleteResult.task!.Id}");
-        break;
-    }
-
+    DeleteTask();
     break;
 
   case "mark-in-progress":
-
-    if (!taskTracker.HasTasks())
-    {
-      Console.WriteLine("Add at leat 1 task");
-      return;
-    }
-
-    if (arguments.Length < 3)
-    {
-      Errors.NoId();
-      return;
-    }
-
-    var markInProgressResult = taskTracker.MarkInProgress(int.Parse(arguments[2]));
-
-    switch (markInProgressResult.operationResult)
-    {
-      case OperationResult.NotFound:
-        Errors.TaskNotFound(arguments[2]);
-        break;
-      case OperationResult.AlreadyInProgress:
-        Errors.CustomError("already in progress");
-        break;
-      case OperationResult.Success:
-        Errors.PrintTaskInfo(markInProgressResult.task!);
-        break;
-    }
-
+    MarkInProgress();
     break;
 
   case "mark-done":
-
-    if (!taskTracker.HasTasks())
-    {
-      Console.WriteLine("Add at leat 1 task");
-      return;
-    }
-
-    if (arguments.Length < 3)
-    {
-      Errors.NoId();
-      return;
-    }
-
-    var markDoneResult = taskTracker.MarkDone(int.Parse(arguments[2]));
-    switch (markDoneResult.operationResult)
-    {
-      case OperationResult.NotFound:
-        Errors.TaskNotFound(arguments[2]);
-        break;
-      case OperationResult.AlreadyDone:
-        Errors.CustomError("already done");
-        break;
-      case OperationResult.Success:
-        Errors.PrintTaskInfo(markDoneResult.task!);
-        break;
-    }
-
+    MarkDone();
     break;
 
   case "list":
-
-    if (!taskTracker.HasTasks())
-    {
-      Console.WriteLine("Add at leat 1 task");
-      return;
-    }
-
-    Status? status = null;
-
-    if (arguments.Length > 2)
-    {
-      status = ParseStatus(arguments[2]);
-      if (status == null)
-      {
-        Console.WriteLine($"invalid {arguments[2]}");
-        return;
-      }
-    }
-
-    var tasks = taskTracker.List(status);
-
-    if (tasks.Count == 0)
-    {
-      Console.WriteLine($"you don't have anything {status}");
-      return;
-    }
-
-    Errors.PrintList(tasks);
+    ListTasks();
     break;
 
   default:
@@ -200,11 +51,204 @@ switch (command)
 
 return;
 
-static Status? ParseStatus(string input)
+void AddTask()
+{
+  if (arguments.Length < 3)
+  {
+    Errors.CustomError("text is missing");
+    return;
+  }
+
+  var addResult = taskManager.AddTask(arguments[2]);
+
+  switch (addResult.operationResult)
+  {
+    case OperationResult.Success:
+      Errors.PrintAddInfo(addResult.task!.Id, addResult.task!.Description);
+      break;
+    case OperationResult.EmptyDescription:
+      Console.WriteLine("task description can't be empty");
+      break;
+  }
+}
+
+void UpdateTask()
+{
+  if (!taskManager.HasTasks())
+  {
+    Console.WriteLine("Add at leat 1 task");
+    return;
+  }
+
+  switch (arguments.Length)
+  {
+    case < 3:
+      Errors.NoId();
+      return;
+    case < 4:
+      Errors.CustomError("text is missing");
+      return;
+  }
+
+  if (!int.TryParse(arguments[2], out var id))
+  {
+    Errors.CustomError("id must be a number");
+    return;
+  }
+
+  var (operationResult, task) = taskManager.UpdateTask(id, arguments[3]);
+
+  switch (operationResult)
+  {
+    case OperationResult.NotFound:
+      Errors.TaskNotFound(arguments[2]);
+      break;
+    case OperationResult.Success:
+      Errors.PrintTaskInfo(task!);
+      break;
+  }
+}
+
+void DeleteTask()
+{
+  if (!taskManager.HasTasks())
+  {
+    Console.WriteLine("Add at leat 1 task");
+    return;
+  }
+
+  if (arguments.Length < 3)
+  {
+    Errors.NoId();
+    return;
+  }
+
+  if (!int.TryParse(arguments[2], out var id))
+  {
+    Errors.CustomError("id must be a number");
+    return;
+  }
+
+  var deleteResult = taskManager.DeleteTask(id);
+
+  switch (deleteResult.operationResult)
+  {
+    case OperationResult.NotFound:
+      Errors.TaskNotFound(arguments[2]);
+      break;
+    case OperationResult.Success:
+      Errors.CustomError($"you removed task: {deleteResult.task!.Id}");
+      break;
+  }
+}
+
+void MarkInProgress()
+{
+  if (!taskManager.HasTasks())
+  {
+    Console.WriteLine("Add at leat 1 task");
+    return;
+  }
+
+  if (arguments.Length < 3)
+  {
+    Errors.NoId();
+    return;
+  }
+
+  if (!int.TryParse(arguments[2], out var id))
+  {
+    Errors.CustomError("id must be a number");
+    return;
+  }
+
+  var markInProgressResult = taskManager.MarkInProgress(id);
+
+  switch (markInProgressResult.operationResult)
+  {
+    case OperationResult.NotFound:
+      Errors.TaskNotFound(arguments[2]);
+      break;
+    case OperationResult.AlreadyInProgress:
+      Errors.CustomError("already in progress");
+      break;
+    case OperationResult.Success:
+      Errors.PrintTaskInfo(markInProgressResult.task!);
+      break;
+  }
+}
+
+void MarkDone()
+{
+  if (!taskManager.HasTasks())
+  {
+    Console.WriteLine("Add at leat 1 task");
+    return;
+  }
+
+  if (arguments.Length < 3)
+  {
+    Errors.NoId();
+    return;
+  }
+  
+  if (!int.TryParse(arguments[2], out var id))
+  {
+    Errors.CustomError("id must be a number");
+    return;
+  }
+
+  var markDoneResult = taskManager.MarkDone(id);
+  switch (markDoneResult.operationResult)
+  {
+    case OperationResult.NotFound:
+      Errors.TaskNotFound(arguments[2]);
+      break;
+    case OperationResult.AlreadyDone:
+      Errors.CustomError("already done");
+      break;
+    case OperationResult.Success:
+      Errors.PrintTaskInfo(markDoneResult.task!);
+      break;
+  }
+}
+
+void ListTasks()
+{
+  if (!taskManager.HasTasks())
+  {
+    Console.WriteLine("Add at leat 1 task");
+    return;
+  }
+
+  Status? status = null;
+
+  if (arguments.Length > 2)
+  {
+    status = ParseStatus(arguments[2]);
+    if (status == null)
+    {
+      Console.WriteLine($"invalid {arguments[2]}");
+      return;
+    }
+  }
+
+  var tasks = taskManager.List(status);
+
+  if (tasks.Count == 0)
+  {
+    Console.WriteLine($"you don't have anything {status}");
+    return;
+  }
+
+  Errors.PrintList(tasks);
+}
+
+Status? ParseStatus(string input)
 {
   return input switch
   {
-    "todo" => Status.Done,
+    "todo" => Status.Todo,
     "in-progress" => Status.InProgress,
     "done" => Status.Done,
     _ => null
